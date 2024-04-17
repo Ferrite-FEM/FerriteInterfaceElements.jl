d143e98df4f2e8bc1f815aa20345f524e6c0352f
David Rollin <d.rollin@tu-braunschweig.de>
2024-04-17 14:20:35 +0200

    Started rework towards a single base interpolation

--- src/cells.jl
+++ src/cells.jl
@@ -2,7 +2,7 @@
     InterfaceCell(here::AbstractCell, there::AbstractCell) <: AbstractCell
 
 An `InterfaceCell` is a cell based on two cells of lower dimension representing the two faces.
-The two base cells need to use the same reference shape and the order of nodes needs to match, e.g.:
+The two base cells need to be of the same type and the order of nodes needs to match, e.g.:
 ```
 1---2 "here"
 4---3 "there"
@@ -14,22 +14,21 @@ InterfaceCell(Line((1,2)), Line((4,3)))
 - `there::AbstractCell`: cell representing the face "there"
 - `nodes`::NTuple: tuple with all node indices in appropriate order: vertex nodes "here", vertex nodes "there", face nodes "here", ...
 """
-struct InterfaceCell{shape, Chere, Cthere, N} <: AbstractCell{shape}
-    here::Chere
-    there::Cthere
+struct InterfaceCell{shape, C, N} <: AbstractCell{shape}
+    here::C
+    there::C
     nodes::NTuple{N,Int}
 
-    function InterfaceCell{shape, Chere, Cthere}(here::Chere, there::Cthere) where {shape<:AbstractRefShape, Chere<:AbstractCell, Cthere<:AbstractCell}
-        sni = get_sides_and_base_indices(Chere, Cthere)
+    function InterfaceCell{shape, C}(here::C, there::C) where {shape<:AbstractRefShape, C<:AbstractCell}
+        sni = get_sides_and_base_indices(C)
         nodes = ntuple( i -> sni[i][1] == :here ? here.nodes[sni[i][2]] : there.nodes[sni[i][2]], length(sni))
-        new{shape, Chere, Cthere, length(nodes)}(here, there, nodes)
+        new{shape, C, length(nodes)}(here, there, nodes)
     end
 end
 
-function InterfaceCell(here::Chere, there::Cthere) where {Chere<:AbstractCell, Cthere<:AbstractCell}
-    @assert getrefshape(here) == getrefshape(there) "For an `InterfaceCell` the underlying cells need to be based on the same shape."
-    shape = get_interface_cell_shape(getrefshape(here))
-    return InterfaceCell{shape, Chere, Cthere}(here, there)
+function InterfaceCell(here::C, there::C) where {baseshape<:AbstractRefShape, C<:AbstractCell{baseshape}}
+    shape = get_interface_cell_shape(baseshape)
+    return InterfaceCell{shape, C}(here, there)
 end
 
 """
@@ -42,38 +41,31 @@ get_interface_cell_shape(::Type{RefLine}) = RefQuadrilateral
 get_interface_cell_shape(::Type{RefTriangle}) = RefPrism
 get_interface_cell_shape(::Type{RefQuadrilateral}) = RefHexahedron
 
-
-Ferrite.vertices(c::InterfaceCell) = (vertices(c.here)..., vertices(c.there)...)
-Ferrite.faces(c::InterfaceCell) = (vertices(c.here), vertices(c.there))
-Ferrite.edges(c::InterfaceCell) = (faces(c.here)..., faces(c.there)...)
-
 """
-    get_sides_and_base_indices(c::InterfaceCell)
-    get_sides_and_base_indices(::AbstractCell, ::AbstractCell)
-    get_sides_and_base_indices(::Type{<:AbstractRefShape}, ::Type{<:AbstractRefShape})
+    get_sides_and_base_indices(::InterfaceCell)
+    get_sides_and_base_indices(::AbstractCell)
+    get_sides_and_base_indices(::Type{<:AbstractCell})
 
 Return a tuple containing tuples of a symbol (:here or :there) and an integer.
 The index of the outer tuple represents the node index.
 In the inner tuple, the symbol represents the side the node is on 
 and the integer represents the nodes index in the base cell.
 """
-get_sides_and_base_indices(c::InterfaceCell) = get_sides_and_base_indices(c.here, c.there)
-get_sides_and_base_indices(::Chere, ::Cthere) where {Chere <: AbstractCell, Cthere <: AbstractCell} = get_sides_and_base_indices(Chere, Cthere)
+get_sides_and_base_indices(::InterfaceCell{<:Any, C}) where {C<: AbstractCell} = get_sides_and_base_indices(C)
+get_sides_and_base_indices(::C) where {C <: AbstractCell} = get_sides_and_base_indices(C)
 
-get_sides_and_base_indices(::Type{Line}, ::Type{Line}) = ((:here,1), (:here,2), (:there,1), (:there,2))
-get_sides_and_base_indices(::Type{QuadraticLine}, ::Type{Line}) = ((:here,1), (:here,2), (:there,1), (:there,2), (:here,3))
-get_sides_and_base_indices(::Type{Line}, ::Type{QuadraticLine}) = ((:here,1), (:here,2), (:there,1), (:there,2), (:there,3))
-get_sides_and_base_indices(::Type{QuadraticLine}, ::Type{QuadraticLine}) = ((:here,1), (:here,2), (:there,1), (:there,2), (:here,3), (:there,3))
+get_sides_and_base_indices(::Type{Line}) = ((:here,1), (:here,2), (:there,1), (:there,2))
+get_sides_and_base_indices(::Type{QuadraticLine}) = ((:here,1), (:here,2), (:there,1), (:there,2), (:here,3), (:there,3))
 
-get_sides_and_base_indices(::Type{Triangle}, ::Type{Triangle}) = ((:here,1), (:here,2), (:here,3), (:there,1), (:there,2), (:there,3))
-get_sides_and_base_indices(::Type{QuadraticTriangle}, ::Type{Triangle}) = ((:here,1), (:here,2), (:here,3), (:there,1), (:there,2), (:there,3), (:here,4), (:here,5), (:here,6))
-get_sides_and_base_indices(::Type{Triangle}, ::Type{QuadraticTriangle}) = ((:here,1), (:here,2), (:here,3), (:there,1), (:there,2), (:there,3), (:there,4), (:there,5), (:there,6))
-get_sides_and_base_indices(::Type{QuadraticTriangle}, ::Type{QuadraticTriangle}) = ((:here,1), (:here,2), (:here,3), (:there,1), (:there,2), (:there,3), (:here,4), (:here,5), (:here,6), (:there,4), (:there,5), (:there,6))
+get_sides_and_base_indices(::Type{Triangle}) = ((:here,1), (:here,2), (:here,3), (:there,1), (:there,2), (:there,3))
+get_sides_and_base_indices(::Type{QuadraticTriangle}) = ((:here,1), (:here,2), (:here,3), (:there,1), (:there,2), (:there,3), (:here,4), (:here,5), (:here,6), (:there,4), (:there,5), (:there,6))
 
-get_sides_and_base_indices(::Type{Quadrilateral}, ::Type{Quadrilateral}) = ((:here,1), (:here,2), (:here,3), (:here,4), (:there,1), (:there,2), (:there,3), (:there,4))
-get_sides_and_base_indices(::Type{QuadraticQuadrilateral}, ::Type{Quadrilateral}) = ((:here,1), (:here,2), (:here,3), (:here,4), (:there,1), (:there,2), (:there,3), (:there,4), (:here,5), (:here,6), (:here,7), (:here,8), (:here,9))
-get_sides_and_base_indices(::Type{Quadrilateral}, ::Type{QuadraticQuadrilateral}) = ((:here,1), (:here,2), (:here,3), (:here,4), (:there,1), (:there,2), (:there,3), (:there,4), (:there,5), (:there,6), (:there,7), (:there,8), (:there,9))
-get_sides_and_base_indices(::Type{QuadraticQuadrilateral}, ::Type{QuadraticQuadrilateral}) = ((:here,1), (:here,2), (:here,3), (:here,4), (:there,1), (:there,2), (:there,3), (:there,4), (:here,5), (:here,6), (:here,7), (:here,8), (:there,5), (:there,6), (:there,7), (:there,8), (:here,9), (:there,9))
+get_sides_and_base_indices(::Type{Quadrilateral}) = ((:here,1), (:here,2), (:here,3), (:here,4), (:there,1), (:there,2), (:there,3), (:there,4))
+get_sides_and_base_indices(::Type{QuadraticQuadrilateral}) = ((:here,1), (:here,2), (:here,3), (:here,4), (:there,1), (:there,2), (:there,3), (:there,4), (:here,5), (:here,6), (:here,7), (:here,8), (:there,5), (:there,6), (:there,7), (:there,8), (:here,9), (:there,9))
+
+Ferrite.vertices(c::InterfaceCell) = (vertices(c.here)..., vertices(c.there)...)
+Ferrite.faces(c::InterfaceCell) = (vertices(c.here), vertices(c.there))
+Ferrite.edges(c::InterfaceCell) = (faces(c.here)..., faces(c.there)...)
 
 ######################################################################
 # Inserting cells into a grid
--- src/interpolations.jl
+++ src/interpolations.jl
@@ -1,47 +1,73 @@
+_ndofs(dofs::Tuple) = sum(idxs -> length(idxs), dofs)
+_nvertexdofs(ip) = _ndofs(Ferrite.vertexdof_indices(ip))
+_nedgedofs(ip) = _ndofs(Ferrite.edgedof_interior_indices(ip))
+_nfacedofs(ip) = _ndofs(Ferrite.facedof_interior_indices(ip))
+_ncelldofs(ip) = _ndofs(Ferrite.celldof_interior_indices(ip))
+
 # The constructors of `InterpolationInfo` require a `VectorizedInterpolation{InterfaceCellInterpolation}`
 # and not an `InterfaceCellInterpolation{VectorizedInterpolation,VectorizedInterpolation}`.
 # To create a `VectorizedInterpolation{InterfaceCellInterpolation}`, `InterfaceCellInterpolation` needs to be a `ScalarInterpolation`.
 
 """
-    InterfaceCellInterpolation(here::ScalarInterpolation, there::ScalarInterpolation) <: ScalarInterpolation
+    InterfaceCellInterpolation(ip::ScalarInterpolation) <: ScalarInterpolation
 
-An `InterfaceCellInterpolation` is an interpolation based on two interpolations on the faces of an `InterfaceCell`.
-If only one interpolation is given, it will be used for both faces.
+An `InterfaceCellInterpolation` is based on a regular interpolation which will be applied 
+to both faces of an `InterfaceCell`.
 
 # Fields
-- `here::ScalarInterpolation`: interpolation on the face "here"
-- `there::ScalarInterpolation`: interpolation on the face "there"
+- `base::ScalarInterpolation`: base interpolation
 """
-struct InterfaceCellInterpolation{shape, IPhere, IPthere} <: ScalarInterpolation{shape, Nothing}
-    here::IPhere
-    there::IPthere
+struct InterfaceCellInterpolation{shape, IP} <: ScalarInterpolation{shape, Nothing}
+    base::IP
 
-    function InterfaceCellInterpolation(here::IPhere, there::IPthere) where {IPhere<:ScalarInterpolation, IPthere<:ScalarInterpolation}
-        @assert getrefshape(here) == getrefshape(there) "For an `InterfaceCellInterpolation` the underlying interpolations need to be based on the same shape."
-        shape = get_interface_cell_shape(getrefshape(here))
-        return new{shape, IPhere, IPthere}(here, there)
+    function InterfaceCellInterpolation(base::IP) where {IP<:ScalarInterpolation}
+        shape = get_interface_cell_shape(getrefshape(base))
+        return new{shape, IP}(base)
     end
 end
 
-function InterfaceCellInterpolation(ip::ScalarInterpolation)
-    return InterfaceCellInterpolation(ip, ip)
+Ferrite.getnbasefunctions(ip::InterfaceCellInterpolation) = 2*getnbasefunctions(ip.base)
+
+Ferrite.adjust_dofs_during_distribution(ip::InterfaceCellInterpolation) = Ferrite.adjust_dofs_during_distribution(ip.base)
+
+Ferrite.n_components(ip::InterfaceCellInterpolation) = n_components(ip.base)
+
+Ferrite.getorder(ip::InterfaceCellInterpolation) = getorder(ip.base)
+Ferrite.getorder(ip::VectorizedInterpolation{<:Any,<:Any,<:Any,<:InterfaceCellInterpolation}) = getorder(ip.ip)
+
+function Ferrite.vertexdof_indices(ip::InterfaceCellInterpolation)
+    here  = Ferrite.vertexdof_indices(ip.base)
+    there = map(v -> map(d -> d + nvertexdofs(ip.base), v), here)
+    return (here..., there...)
 end
 
-Ferrite.getnbasefunctions(ip::InterfaceCellInterpolation) = getnbasefunctions(ip.here) + getnbasefunctions(ip.there)
-
-"""
-    get_n_dofs_on_side(get_dofs::Function, ip::InterfaceCellInterpolation, side::Symbol)
-
-Return the number of DOFs on a `side` (`:here` or `:there`) of an `InterfaceCellInterpolation`.
-The function `get_dofs` specifies which DOFs are considered, e.g. by passing `vertexdof_indices`.
-"""
-function get_n_dofs_on_side(get_dofs::Function, ip::InterfaceCellInterpolation, side::Symbol)
-    baseip = getproperty(ip, side)
-    return length(get_dofs(baseip)) > 0 ? sum(length.(get_dofs(baseip))) : 0
+function Ferrite.edgedof_interior_indices(ip::InterfaceCellInterpolation{<:AbstractRefShape{3}})
+    basedofs = Ferrite.facedof_interior_indices(ip.base)
+    offset = _nvertexdofs(ip.base)
+    here  = map(v -> map(d -> d + offset, v), basedofs)
+    offset += _nfacedofs(ip.base)
+    there = map(v -> map(d -> d + offset, v), basedofs)
+    return (here..., there...)
 end
-function get_n_dofs_on_side(get_dofs::Function, ip::VectorizedInterpolation{<:Any,<:Any,<:Any,<:InterfaceCellInterpolation}, side::Symbol)
-    baseip = getproperty(ip.ip, side)
-    return length(get_dofs(baseip)) > 0 ? sum(length.(get_dofs(baseip)))*n_components(ip) : 0
+
+function Ferrite.facedof_interior_indices(ip::InterfaceCellInterpolation{<:AbstractRefShape{dim}}) where {dim}
+    basedofs = Ferrite.celldof_interior_indices(ip.base)
+    offset = _nvertexdofs(ip.base) + (dim == 3 ? _nedgedofs(ip.base) : 0)
+    here  = map(v -> map(d -> d + offset, v), basedofs)
+    offset += ncelldofs(ip.base)
+    there = map(v -> map(d -> d + offset, v), basedofs)
+    return (here, there)
+end
+
+function Ferrite.default_interpolation(::Type{InterfaceCell{shape, C}}) where {shape<:AbstractRefShape, C} 
+    return InterfaceCellInterpolation(default_interpolation(C))
+end
+
+function Ferrite.default_geometric_interpolation(ip::InterfaceCellInterpolation{<:AbstractRefShape{sdim}, IP}) where {sdim, IP<:ScalarInterpolation}
+    return InterfaceCellInterpolation(ip.base)^sdim
+end
+function Ferrite.default_geometric_interpolation(ip::VectorizedInterpolation{<:Any, <:Any, <:Any, <:InterfaceCellInterpolation})
+    return ip
 end
 
 """
@@ -51,25 +77,25 @@ Return the base function index for an `InterfaceCellInterpolation` given a `side
 and the local base function index `i` on that face.
 """
 function get_interface_index(ip::InterfaceCellInterpolation, side::Symbol, i::Integer)
-    nvhere, nvthere = get_n_dofs_on_side(vertexdof_indices, ip, :here), get_n_dofs_on_side(vertexdof_indices, ip, :there)
-    nfhere, nfthere = get_n_dofs_on_side(facedof_interior_indices, ip, :here), get_n_dofs_on_side(facedof_interior_indices, ip, :there)
-    nchere, ncthere = get_n_dofs_on_side(celldof_interior_indices, ip, :here), get_n_dofs_on_side(celldof_interior_indices, ip, :there)
+    nv = _nvertexdofs(ip.base)
+    nf = _nfacedofs(ip.base)
+    nc = _ncelldofs(ip.base)
     if side == :here
-        if i ≤ nvhere
+        if i ≤ nv
             return i
-        elseif i ≤ nvhere + nfhere
-            return i + nvthere
-        elseif i ≤ nvhere + nfhere + nchere
-            return i + nvthere + nfthere
+        elseif i ≤ nv + nf
+            return i + nv
+        elseif i ≤ nvhere + nf + nc
+            return i + nv + nf
         end
         throw(ArgumentError("No interface index for base index $(i) on side $(side) for interpolation $(ip)."))
     elseif side == :there
-        if i ≤ nvthere
-            return i + nvhere
-        elseif i ≤ nvthere + nfthere
-            return i + nvhere + nfhere
-        elseif i ≤ nvthere + nfthere + ncthere
-            return i + nvhere + nfhere + nchere
+        if i ≤ nv
+            return i + nv
+        elseif i ≤ nv + nf
+            return i + nv + nf
+        elseif i ≤ nv + nf + nc
+            return i + nv + nf + nc
         end
         throw(ArgumentError("No interface index for base index $(i) on side $(side) for interpolation $(ip)."))
     end
@@ -95,46 +121,3 @@ function get_interface_dof_indices(get_dofs::Function, ip::InterfaceCellInterpol
         return broadcast.(get_interface_index, ((ip,),), ((side,),), basedofs)
     end
 end
-
-Ferrite.vertexdof_indices(ip::InterfaceCellInterpolation) = get_interface_dof_indices(vertexdof_indices, ip)
-
-function Ferrite.facedof_indices(ip::InterfaceCellInterpolation)
-    here = (get_interface_dof_indices(vertexdof_indices, ip, :here)...,
-            get_interface_dof_indices(facedof_interior_indices, ip, :here)...,
-            get_interface_dof_indices(celldof_interior_indices, ip, :here)...)
-    there = (get_interface_dof_indices(vertexdof_indices, ip, :there)...,
-             get_interface_dof_indices(facedof_interior_indices, ip, :there)...,
-             get_interface_dof_indices(celldof_interior_indices, ip, :there)...)
-    return Tuple(vcat(collect( [t...] for t in here )...)), Tuple(vcat(collect( [t...] for t in there )...))
-end
-
-Ferrite.facedof_interior_indices(ip::InterfaceCellInterpolation) = get_interface_dof_indices(celldof_interior_indices, ip)
-
-Ferrite.edgedof_indices(ip::InterfaceCellInterpolation) = get_interface_dof_indices(facedof_indices, ip)
-
-Ferrite.edgedof_interior_indices(ip::InterfaceCellInterpolation) = get_interface_dof_indices(facedof_interior_indices, ip)
-
-function Ferrite.adjust_dofs_during_distribution(ip::InterfaceCellInterpolation)
-    return adjust_dofs_during_distribution(ip.here) || adjust_dofs_during_distribution(ip.there) # TODO: Is this really the way to do it?
-end
-
-Ferrite.n_components(ip::InterfaceCellInterpolation) = n_components(ip.here)
-
-function Ferrite.default_interpolation(::Type{InterfaceCell{shape, Chere, Cthere}}) where {shape<:AbstractRefShape, Chere<:AbstractCell, Cthere<:AbstractCell} 
-    return InterfaceCellInterpolation(default_interpolation(Chere), default_interpolation(Cthere))
-end
-
-function Ferrite.default_geometric_interpolation(ip::InterfaceCellInterpolation{<:AbstractRefShape{sdim}, IPhere, IPthere}
-    ) where {sdim, IPhere<:ScalarInterpolation, IPthere<:ScalarInterpolation}
-    return InterfaceCellInterpolation(ip.here, ip.there)^sdim
-end
-function Ferrite.default_geometric_interpolation(ip::InterfaceCellInterpolation{<:AbstractRefShape{sdim}, IPhere, IPthere}
-    ) where {sdim, IPhere<:VectorizedInterpolation, IPthere<:VectorizedInterpolation}
-    return InterfaceCellInterpolation(ip.here.ip, ip.there.ip)^sdim
-end
-function Ferrite.default_geometric_interpolation(ip::VectorizedInterpolation{<:Any, <:Any, <:Any, <:InterfaceCellInterpolation})
-    return ip
-end
-
-Ferrite.getorder(ip::InterfaceCellInterpolation) = getorder(ip.here) == getorder(ip.there) ? getorder(ip.here) : (getorder(ip.here), getorder(ip.there))
-Ferrite.getorder(ip::VectorizedInterpolation{<:Any,<:Any,<:Any,<:InterfaceCellInterpolation}) = getorder(ip.ip)
