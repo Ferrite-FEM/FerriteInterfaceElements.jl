9673332827cce50d050af82d907d7e85965deeee
Knut Andreas <knutam@gmail.com>
2025-05-02 16:37:59 +0200

    Minimize diff 1

--- src/FerriteInterfaceElements.jl
+++ src/FerriteInterfaceElements.jl
@@ -18,22 +18,25 @@ import OrderedCollections: OrderedSet
 import StaticArraysCore: SMatrix, SVector
 
 include("cells.jl")
-export InterfaceCell
-
 include("interpolations.jl")
-export InterfaceCellInterpolation
-
 include("cellvalues.jl")
-export InterfaceCellValues,
-    get_side_and_baseindex,
-    shape_value_average, shape_gradient_average,
-    shape_value_jump, shape_gradient_jump,
-    function_value_average, function_gradient_average,
-    function_value_jump, function_gradient_jump,
-    midplane_rotation,
-    getdetJdV_average
-
 include("grid.jl")
-export insert_interfaces
+
+export
+    InterfaceCell,
+    InterfaceCellInterpolation,
+    InterfaceCellValues,
+    get_side_and_baseindex,
+    shape_value_average,
+    shape_gradient_average,
+    shape_value_jump,
+    shape_gradient_jump,
+    function_value_average,
+    function_gradient_average,
+    function_value_jump,
+    function_gradient_jump,
+    getdetJdV_average,
+    midplane_rotation,
+    insert_interfaces
 
 end
