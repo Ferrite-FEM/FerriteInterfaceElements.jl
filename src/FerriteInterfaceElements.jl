b9707229f40b8e90edbf541d222986277d8eb450
Knut Andreas Meyer <knutam@gmail.com>
2026-07-06 11:11:35 +0200

    Minimize diff from main for including rotation (#28)
    * Include rotation matrix in cell values
    
    * Update test/test_cellvalues.jl
    
    Co-authored-by: Knut Andreas Meyer <knutam@gmail.com>
    
    * Updated computation and test of R
    
    * Minor improvement of tests
    
    * fixed correct number of quad points
    
    * Fix function for line based interface and added tests for cv
    
    * Switch from Bool to Val{} in cv constructors
    
    * Minimize diff 1
    
    * Minimize diff
    
    * Minimize test diff
    
    * Switched to MixedTensor for embedded elements
    
    * Remove dependency on StaticArraysCore
    
    ---------
    
    Co-authored-by: David Rollin <d.rollin@tu-braunschweig.de>

--- src/FerriteInterfaceElements.jl
+++ src/FerriteInterfaceElements.jl
@@ -1,11 +1,10 @@
 module FerriteInterfaceElements
 
-import Ferrite
-import Ferrite: AbstractCell, AbstractRefShape, AbstractCellValues,
+import Ferrite: Ferrite, AbstractCell, AbstractRefShape, AbstractCellValues,
     RefLine, RefQuadrilateral, RefTriangle, RefPrism, RefHexahedron,
     Line, QuadraticLine, Triangle, QuadraticTriangle, Quadrilateral, QuadraticQuadrilateral, Tetrahedron, Hexahedron,
     ScalarInterpolation, VectorizedInterpolation, Lagrange,
-    CellValues, QuadratureRule, CellCache, Grid, ExclusiveTopology, FacetIndex, Vec,
+    CellValues, QuadratureRule, CellCache, Grid, ExclusiveTopology, FacetIndex, Vec, Tensor, MixedTensor2,
     getnbasefunctions, getngeobasefunctions, getorder, n_components, getrefshape,
     vertexdof_indices, edgedof_interior_indices, facedof_interior_indices, volumedof_interior_indices,
     vertices, facets, edges,
@@ -13,8 +12,10 @@ import Ferrite: AbstractCell, AbstractRefShape, AbstractCellValues,
     checkbounds, checkquadpoint, function_value_init, function_gradient_init,
     shape_value_type, shape_gradient_type,
     reinit!, getnquadpoints, getdetJdV, shape_value, shape_gradient, function_value, function_gradient,
-    getcellset, getcells, getneighborhood
+    getcellset, getcells, getneighborhood,
+    ×, norm
 import OrderedCollections: OrderedSet
+#import StaticArraysCore: SMatrix, SVector
 
 include("cells.jl")
 include("interpolations.jl")
@@ -35,6 +36,7 @@ export
     function_value_jump,
     function_gradient_jump,
     getdetJdV_average,
+    midplane_rotation,
     insert_interfaces
 
 end
--- test/test_cellvalues.jl
+++ test/test_cellvalues.jl
@@ -36,4 +36,38 @@
         @test all(abs.(function_gradient_jump(cv, qp, u)) .≤ 1e-14)
         @test getdetJdV_average(cv, qp) == (getdetJdV(cv.here, qp) + getdetJdV(cv.there, qp)) / 2
     end
+
+    qr  = QuadratureRule{RefTriangle}(2)
+    ip = InterfaceCellInterpolation(Lagrange{RefTriangle, 1}())
+    cv = InterfaceCellValues(qr, ip; include_R=true)
+    @inferred InterfaceCellValues(qr, ip) # default type stable 
+    @inferred InterfaceCellValues(qr, ip; include_R=Val(false)) # no rotation type stable with Val
+    @inferred InterfaceCellValues(qr, ip; include_R=Val(true)) # with rotation type stable with Val.
+    
+    x  = repeat([Vec{3}((1.0,0.0,0.0)), Vec{3}((0.0,1.0,0.0)), Vec{3}((0.0,0.0,1.0))], 2)
+    n  = Vec{3}(( sqrt(1/3),  sqrt(1/3),   sqrt(1/3)))
+    t₁ = Vec{3}(( sqrt(1/2),  0.0,        -sqrt(1/2)))
+    t₂ = Vec{3}((-sqrt(1/6), 2*sqrt(1/6), -sqrt(1/6)))
+    reinit!(cv, x)
+    for qp in 1:getnquadpoints(cv)
+        R = midplane_rotation(cv, qp)
+        @test tdot(R) ≈ one(R) # Fails e.g. when dx/dξ₁ not perpendicular to dx/ξ₂
+        @test R⋅Vec{3}((1.0,0.0,0.0)) ≈ t₁
+        @test R⋅Vec{3}((0.0,1.0,0.0)) ≈ t₂
+        @test R⋅Vec{3}((0.0,0.0,1.0)) ≈ n
+    end
+
+    qr  = QuadratureRule{RefLine}(2)
+    ip = InterfaceCellInterpolation(Lagrange{RefLine, 1}())
+    cv = InterfaceCellValues(qr, ip; include_R=true)
+    x  = repeat([Vec{2}((1.0,0.0)), Vec{2}((0.0,1.0))], 2)
+    n  = Vec{2}((-sqrt(1/2), -sqrt(1/2)))
+    t  = Vec{2}((-sqrt(1/2),  sqrt(1/2)))
+    reinit!(cv, x)
+    for qp in 1:getnquadpoints(cv)
+        R = midplane_rotation(cv, qp)
+        @test tdot(R) ≈ one(R) # Fails e.g. when dx/dξ₁ not perpendicular to dx/ξ₂
+        @test R⋅Vec{2}((1.0,0.0)) ≈ t
+        @test R⋅Vec{2}((0.0,1.0)) ≈ n
+    end
 end
