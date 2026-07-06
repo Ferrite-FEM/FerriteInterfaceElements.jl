d92fbc2bd12fe9c424728e9f242cb45d4c51e6ec
termi-official <termi-official@users.noreply.github.com>
2026-07-06 13:20:23 +0200

    Forgot to save

--- test/test_vtk_export.jl
+++ test/test_vtk_export.jl
@@ -1,10 +1,5 @@
 using Ferrite, FerriteInterfaceElements, OrderedCollections
 
-getinterfaceshape(cell::Triangle) = RefLine
-getinterfaceshape(cell::Quadrilateral) = RefLine
-getinterfaceshape(cell::Tetrahedron) = RefTriangle
-getinterfaceshape(cell::Hexahedron) = RefQuadrilateral
-
 @testset "VTK Export Smoke Test" begin
     mktempdir() do tmp
         for grid in [
@@ -20,7 +15,7 @@ getinterfaceshape(cell::Hexahedron) = RefQuadrilateral
             set_bulk = union(getcellset(grid2, "A"), getcellset(grid2, "B"))
             add!(SubDofHandler(dh, set_bulk),      :u, Lagrange{Ferrite.getrefshape(grid.cells[1]), 1}())
             set_interface = getcellset(grid2, "interfaces")
-            add!(SubDofHandler(dh, set_interface), :u, InterfaceCellInterpolation(Lagrange{getinterfaceshape(grid.cells[1]), 1}()))
+            add!(SubDofHandler(dh, set_interface), :u, InterfaceCellInterpolation(Lagrange{FerriteInterfaceElements.getinterfaceshape(grid.cells[1]), 1}()))
             close!(dh)
             VTKGridFile("debug.vtu", grid2) do vtk
                 Ferrite.write_solution(vtk, dh, rand(ndofs(dh)))
