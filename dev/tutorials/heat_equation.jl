a89c67aadd14b4e06efa7d8e091d89a57e186588
Documenter.jl <documenter@juliadocs.github.io>
2026-09-07 13:13:36 +0000

    build based on a998898

--- dev/tutorials/heat_equation.jl
+++ dev/tutorials/heat_equation.jl
@@ -107,7 +107,7 @@ function get_nodal_temperatures(u::AbstractVector, dh::DofHandler)
     bulkcells = union( getcellset(grid, "inclusions"), getcellset(grid, "matrix") )
     uₙ = zeros(length(grid.nodes))
     for cc in CellIterator(dh, bulkcells)
-        cell = getcells(grid, cc.cellid)
+        cell = getcells(grid, cellid(cc))
         dofs = celldofs(cc)
         nodes = [cell.nodes...]
         uₙ[nodes] .= u[dofs]
